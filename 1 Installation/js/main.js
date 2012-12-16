0bd4bb32a695a307a70c120dea69375bae3ec250
Mohit Jain <jain.mohit27@gmail.com>
2012-12-16 21:56:38 +0530

    Added - sample code for basic installation for backbone js

---
+++ 1 Installation/js/main.js
@@ -0,0 +1,11 @@
+var Person = function(config) {
+    this.name = config.name;
+    this.age = config.age;
+    this.occupation = congif.occupation;
+
+}
+
+Person.prototype.work = function(){
+ return this.name + " is working. "
+
+}
